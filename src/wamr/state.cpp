534efefa0e5c76233f7e6cfa3b66f388c19b83dc
Tianyu <tiqi@student.unimelb.edu.au>
2024-04-03 07:53:02 +0000

    feat(stream): Add function state v2
    Add the partitioned stateful concept and refine the function state class.

--- src/wamr/state.cpp
+++ src/wamr/state.cpp
@@ -1,4 +1,5 @@
 #include <faabric/executor/ExecutorContext.h>
+#include <faabric/planner/PlannerClient.h>
 #include <faabric/proto/faabric.pb.h>
 #include <faabric/util/logging.h>
 #include <wamr/WAMRWasmModule.h>
@@ -92,35 +93,47 @@ static void __faasm_push_state_wrapper(wasm_exec_env_t exec_env, char* key)
 }
 
 /**
- * Read state for the given key into the buffer provided.
- *
- * Returns size of the state if buffer length is zero.
+ * Register the parititoned stateful function and write the initial data to
+ * state server.
  */
-static int32_t __faasm_read_function_state_wrapper(wasm_exec_env_t exec_env,
-                                                   char* buffer,
-                                                   int32_t bufferLen)
+static void __faasm_create_function_state_wrapper(wasm_exec_env_t exec_env,
+                                                  char* buffer,
+                                                  long bufferLen,
+                                                  char* inputKey,
+                                                  char* stateKey)
 {
-    // If buffer is nullptr, flag is true
     std::string user = ExecutorContext::get()->getMsg().user();
     std::string func = ExecutorContext::get()->getMsg().function();
     int32_t parallelismId = ExecutorContext::get()->getMsg().parallelismid();
-    SPDLOG_DEBUG("S - faasm_read_function_state {} parallelism {} <len> {}",
+    SPDLOG_DEBUG("S - faasm_create_func_state - {}/{}-{} input {}/ state{}",
+                 user,
                  func,
                  parallelismId,
-                 bufferLen);
-    if (bufferLen == 0) {
-        // If buffer len is zero, just need to get the state size
-        SPDLOG_DEBUG("S - faasm_read_function_state get the state size");
-        faabric::state::State& state = faabric::state::getGlobalState();
-        return (int32_t)state.getFunctionStateSize(user, func, parallelismId);
-    } else {
-        auto fs = faabric::state::getGlobalState().getFS(
-          user, func, parallelismId, bufferLen);
-        fs->get(reinterpret_cast<uint8_t*>(buffer));
-        return fs->size();
+                 inputKey,
+                 stateKey);
+    // Create and set the data
+    auto fs = faabric::state::getGlobalState().getFS(
+      user, func, parallelismId, bufferLen);
+    fs->set(reinterpret_cast<uint8_t*>(buffer), bufferLen);
+    // Register the function state to the state
+    if (stateKey != nullptr) {
+        if (inputKey == nullptr) {
+            throw std::runtime_error("inputKey isn't set, but stateKey is set");
+        }
+        fs->setPartitionKey(std::string(stateKey));
     }
-
-    return 0;
+    // Register the function state to the planner
+    auto regReq = std::make_shared<faabric::FunctionStateRegister>();
+    regReq->set_user(user);
+    regReq->set_func(func);
+    regReq->set_isparition(false);
+    if (inputKey != nullptr && stateKey != nullptr) {
+        regReq->set_isparition(true);
+        regReq->set_pinputkey(inputKey);
+        regReq->set_pstatekey(stateKey);
+    }
+    auto& plannerCli = faabric::planner::getPlannerClient();
+    plannerCli.registerFunctionState(regReq);
 }
 
 /**
@@ -134,15 +147,52 @@ static void __faasm_write_function_state_wrapper(wasm_exec_env_t exec_env,
     std::string user = ExecutorContext::get()->getMsg().user();
     std::string func = ExecutorContext::get()->getMsg().function();
     int32_t parallelismId = ExecutorContext::get()->getMsg().parallelismid();
-    // SPGLOG_DEBUG the parallelismId
-    SPDLOG_DEBUG("S - faasm_write_function_state - {} parallelism {}",
-                 func,
-                 parallelismId);
-
+    SPDLOG_DEBUG(
+      "S - faasm_write_function_state - {}/{}-{}", user, func, parallelismId);
+    // Create and set the data
     auto fs = faabric::state::getGlobalState().getFS(
       user, func, parallelismId, bufferLen);
-
     fs->set(reinterpret_cast<uint8_t*>(buffer), bufferLen);
+}
+
+/**
+ * Read state for the given key into the buffer provided.
+ * RULES:
+ * Reading Size :
+ * If local storage has master copy, return its size.
+ * Otherwise get the master from remote and return it from remote.
+ * If not registered, return 0. If registered remote don't have, delete the Redis
+ * key and return 0.
+ */
+static int32_t __faasm_read_function_state_wrapper(wasm_exec_env_t exec_env,
+                                                   char* buffer,
+                                                   int32_t bufferLen,
+                                                   char* inputKeys)
+{
+    // If buffer is nullptr, flag is true
+    std::string user = ExecutorContext::get()->getMsg().user();
+    std::string func = ExecutorContext::get()->getMsg().function();
+    int32_t parallelismId = ExecutorContext::get()->getMsg().parallelismid();
+    SPDLOG_DEBUG(
+      "S - faasm_read_function_state - {}/{}-{}", user, func, parallelismId);
+    if (inputKeys != nullptr) {
+        SPDLOG_DEBUG("S - faasm_read_function_state - inputKeys: {}",
+                     inputKeys);
+    }
+    if (bufferLen == 0) {
+        // If buffer len is zero, just need to get the state size
+        SPDLOG_DEBUG("S - faasm_read_function_state get the state size");
+        faabric::state::State& state = faabric::state::getGlobalState();
+        return (int32_t)state.getFunctionStateSize(user, func, parallelismId);
+    } else {
+        // TODO - only read the input keys for partitioned stateful.
+        auto fs = faabric::state::getGlobalState().getFS(
+          user, func, parallelismId, bufferLen);
+        fs->get(reinterpret_cast<uint8_t*>(buffer));
+        return fs->size();
+    }
+
+    return 0;
 }
 
 static NativeSymbol ns[] = {
@@ -150,8 +200,9 @@ static NativeSymbol ns[] = {
     REG_NATIVE_FUNC(__faasm_read_state_ptr, "($i)i"),
     REG_NATIVE_FUNC(__faasm_write_state, "($$i)"),
     REG_NATIVE_FUNC(__faasm_push_state, "($)"),
-    REG_NATIVE_FUNC(__faasm_read_function_state, "($i)i"),
+    REG_NATIVE_FUNC(__faasm_create_function_state, "($i$$)"),
     REG_NATIVE_FUNC(__faasm_write_function_state, "($i)"),
+    REG_NATIVE_FUNC(__faasm_read_function_state, "($i$)i"),
 };
 
 uint32_t getFaasmStateApi(NativeSymbol** nativeSymbols)
