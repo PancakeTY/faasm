cd3a127f40c9db2343bcf0169f9c815bd875d518
Tianyu Qi <tiqi@student.unimelb.edu.au>
2024-05-21 05:50:08 +0000

    feat(docker): Custom dockerfile and entrypoint
    Change the dockerfile to local github repo and entrypoint to check if
    the worker code is updated.

---
+++ bin/entrypoint_worker_stream.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+# Record the current directory
+THIS_DIR=$(dirname $(readlink -f $0))
+
+# Change to the Faasm code directory
+cd /usr/local/code/faasm 
+
+# Check for updates in the repository and rebuild if necessary
+echo "Checking for updates in the Faasm repository..."
+git fetch origin state
+LOCAL_COMMIT=$(git rev-parse HEAD)
+REMOTE_COMMIT=$(git rev-parse origin/state)
+
+if [ "$LOCAL_COMMIT" != "$REMOTE_COMMIT" ]; then
+    echo "New changes detected. Updating the repository and rebuilding the pool_runner..."
+    git pull origin state
+    source venv/bin/activate
+    inv dev.cc pool_runner
+else
+    echo "The repository is already up to date."
+fi
+
+# Run codegen
+cd $THIS_DIR
+$THIS_DIR/entrypoint_codegen.sh
+
+# Start hoststats
+nohup hoststats start > /var/log/hoststats.log 2>&1 &
+
+# Set up isolation
+pushd /usr/local/code/faasm >> /dev/null
+
+echo "Setting up cgroup"
+./bin/cgroup.sh
+
+echo "Setting up namespaces"
+./bin/netns.sh ${MAX_NET_NAMESPACES}
+
+popd >> /dev/null
+
+# Continue with normal command
+exec "$@"
---
+++ docker/worker-stream.dockerfile
@@ -0,0 +1,31 @@
+ARG FAASM_VERSION
+ARG FAASM_SGX_PARENT_SUFFIX
+FROM tqiunimelb/base-stream:0.0.2
+
+# Build the worker binary
+ARG FAASM_SGX_MODE
+RUN cd /usr/local/code/faasm \
+    && ./bin/create_venv.sh \
+    && source venv/bin/activate \
+    && inv dev.cmake --build Release --sgx ${FAASM_SGX_MODE} \
+    && inv dev.cc codegen_shared_obj \
+    && inv dev.cc codegen_func \
+    && inv dev.cc pool_runner
+
+WORKDIR /build/faasm
+
+# Install hoststats
+RUN pip3 install hoststats==0.1.0
+
+# Set up entrypoint (for cgroups, namespaces etc.)
+COPY bin/entrypoint_codegen.sh /entrypoint_codegen.sh
+COPY bin/entrypoint_worker_stream.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+# Create user with dummy uid required by Python
+RUN groupadd -g 1000 faasm
+RUN useradd -u 1000 -g 1000 faasm
+
+ENTRYPOINT ["/entrypoint.sh"]
+CMD "/build/faasm/bin/pool_runner"
+
